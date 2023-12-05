0d4d5d066967419dfe9de931aca0a57c929fed9e
Patrick Piper <ppiper@infoblox.com>
2023-12-05 11:31:02 -0700

    refactor: Remove IbxLogger class from ibx_logger.py
    The IbxLogger class is removed from ibx_logger.py because it was found
    to be redundant and unused in the current implementation. The removal
    includes the deletion of IbxLogger related attributes, parameters,
    methods, and their detailed documentation in the script. Furthermore,
    Literal type imported from typing module is no longer necessary, hence
    it is also removed from the import line.

--- src/ibx_tools/logger/ibx_logger.py
+++ src/ibx_tools/logger/ibx_logger.py
@@ -5,12 +5,9 @@ custom logging module
 import logging
 import os
 from logging.handlers import RotatingFileHandler
-from typing import Literal, Optional
+from typing import Optional
 
 import coloredlogs
-
-LogHandlers = Literal['ALL', 'CONSOLE', 'FILE', 'SYSLOG']
-LogLevel = Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 
 
 class CallCounted:
@@ -23,139 +20,6 @@ class CallCounted:
     def __call__(self, *args, **kwargs):
         self.counter += 1
         return self.method(*args, **kwargs)
-
-
-class IbxLogger(logging.Logger):
-    """
-    IbxLogger class is a subclass of the logging.Logger class from the logging module. It provides additional functionality for logging to console and file.
-
-    Attributes:
-        log_format (str): The log format string.
-
-    Parameters:
-        name (str): The name of the logger. Default is 'root'.
-        level (LogLevel | int): The logging level. Default is 'INFO'.
-        console_log (bool): Whether to log to console. Default is None.
-        filename (str): The name of the log file. Default is None.
-        filemode (Literal['w', 'a']): The file mode for writing logs. Default is 'w'.
-        max_filesize (int): The maximum size of each log file in bytes. Default is None.
-        num_files (int): The maximum number of log files. Default is None.
-
-    Methods:
-        set_level(level: LogLevel | int, handler: LogHandlers = 'ALL') -> None:
-            Sets the logging level for specific handlers.
-
-        __add_rotating_file_handler() -> None:
-            Adds a rotating file handler to the logger.
-
-        __add_file_handler() -> None:
-            Adds a file handler to the logger.
-
-        add_console_handler() -> None:
-            Initializes a colored console logger.
-
-    Note:
-        - The log file path can be set using the 'IBX_LOGDIR' environment variable.
-        - If console_log is set to True, logging will be enabled to the console.
-        - If filename, filemode, max_filesize, and num_files are provided, a rotating file handler will be added to the logger.
-          Otherwise, if filename and filemode are provided, a regular file handler will be added to the logger.
-
-    Usage:
-        logger = IbxLogger(name='mylogger', level='DEBUG', console_log=True, filename='app.log', filemode='a', max_filesize=1024, num_files=5)
-        logger.set_level('ERROR', 'CONSOLE')
-        logger.add_console_handler()
-    """
-    log_format = '%(asctime)s [%(filename)s:%(lineno)d] %(levelname)s %(message)s'
-
-    def __init__(self,
-                 name: str = None,
-                 level: LogLevel | int = 'INFO',
-                 console_log: bool = None,
-                 filename: str = None,
-                 filemode: Literal['w', 'a'] = 'w',
-                 max_filesize: int = None,
-                 num_files: int = None,
-                 ):
-        super().__init__(name=name, level=level)
-        self.console_log = console_log
-        self.filename = filename
-        self.filemode = filemode
-        self.filesize = max_filesize
-        self.num_files = num_files
-
-        # feature - logfile path
-        if os.getenv('IBX_LOGDIR') and self.filename:
-            if not os.path.exists(os.getenv('IBX_LOGDIR')):
-                try:
-                    os.makedirs(os.getenv('IBX_LOGDIR'))
-                except FileExistsError as err:
-                    logging.warning(err)
-                except OSError as err:
-                    logging.warning(err)
-            self.filename = os.path.join(os.getenv('IBX_LOGDIR'), self.filename)
-
-        if self.console_log:
-            self.add_console_handler()
-
-        if self.filename and self.filemode and self.filesize and self.num_files:
-            self.__add_rotating_file_handler()
-        elif self.filename and self.filemode:
-            self.__add_file_handler()
-
-    def set_level(self, level: LogLevel | int, handler: LogHandlers = 'ALL'):
-        if level.isnumeric():
-            self.level = level
-        else:
-            self.level = logging.getLevelName(level)
-        for handle in self.handlers:
-            if (
-                    isinstance(handle, coloredlogs.StandardErrorHandler) and
-                    handler in ['ALL', 'CONSOLE']
-            ):
-                handle.setLevel(level)
-            elif (
-                    (isinstance(handle, logging.FileHandler) or
-                     isinstance(handle, logging.handlers.RotatingFileHandler)) and
-                    handler in ['ALL', 'FILE']
-            ):
-                handle.setLevel(level)
-
-    def __add_rotating_file_handler(self):
-        lfh = RotatingFileHandler(
-            filename=self.filename,
-            maxBytes=self.filesize,
-            backupCount=self.num_files
-        )
-
-        lfh.setFormatter(logging.Formatter(IbxLogger.log_format))
-        lfh.setLevel(self.level)
-        self.addHandler(lfh)
-
-    def __add_file_handler(self):
-        lfh = logging.FileHandler(
-            filename=self.filename,
-            mode=self.filemode,
-        )
-
-        lfh.setFormatter(logging.Formatter(IbxLogger.log_format))
-        lfh.setLevel(self.level)
-        self.addHandler(lfh)
-
-    def add_console_handler(self):
-        """
-        Initialize a colored console logger
-
-        """
-        if not self.level:
-            self.level = logging.INFO
-        coloredlogs.DEFAULT_FIELD_STYLES['filename'] = dict(color='blue')
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(fmt=logging.Formatter(IbxLogger.log_format))
-        coloredlogs.install(
-            logger=self,
-            level=self.level,
-            fmt=IbxLogger.log_format
-        )
 
 
 log_levels = {
