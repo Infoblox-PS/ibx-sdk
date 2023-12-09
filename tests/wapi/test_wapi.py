c5d140ecc82455ce11baaf860e9d9f879ff0c47b
Patrick Piper <ppiper@infoblox.com>
2023-12-09 08:18:47 -0700

    test: Add tests for WAPI instantiation
    This commit introduces new test cases for the instantiation of the WAPI
    class. It verifies the correct working of instantiation with no
    properties, positional arguments, named arguments, and dictionary of
    arguments.

---
+++ tests/wapi/test_wapi.py
@@ -0,0 +1,33 @@
+"""
+WAPI test module
+"""
+from ibx_tools.nios.wapi import WAPI
+
+
+def test_instantiate_wapi_without_properties():
+    wapi = WAPI()
+    assert wapi.grid_mgr is None
+    assert wapi.wapi_ver == '2.5'
+    assert wapi.ssl_verify is False
+
+
+def test_instantiate_wapi_with_positional_arguments():
+    wapi = WAPI('192.168.1.2', '2.12', '/path/to/certfile')
+    assert wapi.grid_mgr == '192.168.1.2'
+    assert wapi.wapi_ver == '2.12'
+    assert wapi.ssl_verify == '/path/to/certfile'
+
+
+def test_instantiate_wapi_with_named_arguments():
+    wapi = WAPI(grid_mgr='192.168.1.2', wapi_ver='2.12', ssl_verify='/path/to/certfile')
+    assert wapi.grid_mgr == '192.168.1.2'
+    assert wapi.wapi_ver == '2.12'
+    assert wapi.ssl_verify == '/path/to/certfile'
+
+
+def test_instantiate_wapi_with_dictionary_of_arguments():
+    props = dict(grid_mgr='192.168.1.2', wapi_ver='2.12', ssl_verify='/path/to/certfile')
+    wapi = WAPI(**props)
+    assert wapi.grid_mgr == '192.168.1.2'
+    assert wapi.wapi_ver == '2.12'
+    assert wapi.ssl_verify == '/path/to/certfile'
