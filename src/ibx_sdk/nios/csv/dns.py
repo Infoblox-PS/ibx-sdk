3ec1ee82a9682b88db9b2f7845776bdea2c279bc
Patrick Piper <ppiper@infoblox.com>
2025-01-05 10:53:42 -0700

    Refactor DNS models to improve type safety and clarity.
    Updated field definitions in DNS-related classes to use more precise
    types, including `Optional` and typed lists, and added descriptions for
    better documentation. Replaced `alias` with `serialization_alias` to
    align with serialization standards. Improved code readability and
    ensured consistency across all DNS model classes.

--- src/ibx_sdk/nios/csv/dns.py
+++ src/ibx_sdk/nios/csv/dns.py
@@ -1,34 +1,55 @@
-from .enums import ZoneFormatTypeEnum, ImportActionEnum
+from typing import Optional, List
 
+from netaddr.ip import IPAddress
 from pydantic import BaseModel, Field, ConfigDict, PositiveInt
+
+from .enums import ZoneFormatTypeEnum, ImportActionEnum
 
 
 class MemberDns(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    memberdns: str = Field(alias="header-memberdns", default="memberdns")
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
-    parent: str
-    dns_over_mgmt: bool | None = None
-    dns_over_lan2: bool | None = None
-    minimal_response: bool | None = None
-    forwarders_only: bool | None = None
-    allow_forwarder: str | None = None  # list of ip addresses
-    member_view_nats: str | None = None  # example: external_dns/INTERFACE/10.10.10.10
-    enable_notify_source_port: bool | None = None
-    notify_source_port: PositiveInt | None = None
-    enable_query_source_port: bool | None = None
-    query_source_port: PositiveInt | None = None
-    lame_ttl: PositiveInt | None = None
-    auto_sort_views: bool | None = None
-    member_views: str | None = None
-    allow_transfer: str | None = None  # Example: NACL1 or "12.0.0.12/Deny,1234::/64/Allow"
-    excluded_servers: str | None = None
-    zone_transfer_format_option: str | None = None
-    recursion_enabled: bool | None = None
-    allow_query: str | None = None  # ACL
-    allow_recursive_query: str | None = None  # ACL
-    limit_concurrent_recursive_clients: bool | None = None
+    memberdns: str = Field(
+        "memberdns",
+        frozen=True,
+        serialization_alias="header-memberdns", description="CSV header for memberdns"
+    )
+    import_action: Optional[ImportActionEnum] = Field(
+        serialization_alias="import-action", default=None, description="CSV custom import action"
+    )
+    parent: str = Field(..., description="Parent FQDN Member")
+    dns_over_mgmt: Optional[bool] = Field(None, description="DNS over management interface")
+    dns_over_lan2: Optional[bool] = Field(None, description="DNS over LAN2 interface")
+    minimal_response: Optional[bool] = Field(None, description="Minimal response flag")
+    forwarders_only: Optional[bool] = Field(None, description="Forwarders only flag")
+    allow_forwarder: Optional[List[IPAddress]] = Field(None, description="List of forwarders")
+    member_view_nats: Optional[List[str]] = Field(
+        None, description="List of member view NATs VIEW/INTERFACE/IP"
+    )
+    enable_notify_source_port: Optional[bool] = Field(None, description="Enable notify source port")
+    notify_source_port: Optional[PositiveInt] = Field(None, description="Notify source port")
+    enable_query_source_port: Optional[bool] = Field(None, description="Enable query source port")
+    query_source_port: Optional[PositiveInt] = Field(None, description="Query source port")
+    lame_ttl: Optional[PositiveInt] = Field(None, description="Lame TTL")
+    auto_sort_views: Optional[bool] = Field(None, description="Auto sort views flag")
+    member_views: Optional[List[str]] = Field(None, description="Member views")
+    allow_transfer: Optional[List[str]] = Field(
+        None, description="List of allowed transfer servers ITEM/Allow or ITEM/Deny"
+    )
+    excluded_servers: Optional[List[IPAddress]] = Field(
+        None, description="List of excluded transfer servers"
+    )
+    zone_transfer_format_option: Optional[str] = Field(
+        None, description="Zone transfer format option"
+    )
+    recursion_enabled: Optional[bool] = Field(None, description="Recursion enabled flag")
+    allow_query: Optional[List[str]] = Field(None, description="List of allowed query servers")
+    allow_recursive_query: Optional[List[str]] = Field(
+        None, description="List of allowed recursive query servers"
+    )
+    limit_concurrent_recursive_clients: Optional[bool] = Field(
+        None, description="Limit concurrent recursive clients flag"
+    )
     concurrent_recursive_clients: PositiveInt | None = Field(default=1000)
     allow_update: str | None = None
     allow_gss_tsig_zone_updates: bool | None = None
@@ -78,13 +99,14 @@ class MemberDns(BaseModel):
 class AuthZone(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    authzone: str = Field(alias="header-authzone", default="authzone")
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    authzone: str = Field(serialization_alias="header-authzone", default="authzone")
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     fqdn: str
     zone_format: ZoneFormatTypeEnum
     view: str | None = None
     prefix: str | None = None
-    new_prefix: str | None = Field(alias="_new_prefix", default=None)
+    new_prefix: str | None = Field(serialization_alias="_new_prefix", default=None)
     is_multimaster: bool | None = None
     grid_primaries: str | None = None  # list of FQDN/IS_STEALTH flag
     external_primaries: str | None = None  # Example: "ext1.test.com/1.1.1.1/FALSE"
@@ -121,8 +143,9 @@ class AuthZone(BaseModel):
 class ForwardZone(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    forwardzone: str = Field(alias="header-forwardzone", default="forwardzone")
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    forwardzone: str = Field(serialization_alias="header-forwardzone", default="forwardzone")
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     fqdn: str
     view: str | None = None
     zone_format: ZoneFormatTypeEnum
@@ -146,8 +169,9 @@ class ForwardZone(BaseModel):
 class StubZone(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    stubzone: str = Field(alias='header-stubzone', default='stubzone')
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    stubzone: str = Field(serialization_alias='header-stubzone', default='stubzone')
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     fqdn: str
     view: str | None = None
     zone_format: ZoneFormatTypeEnum
@@ -155,8 +179,8 @@ class StubZone(BaseModel):
     disabled: bool | None = None
     comment: str | None = None
     disable_forwarding: bool | None = None
-    stub_from: str | None = None # list of [<fqdn>/<ip>,...]
-    stub_members: str | None = None # list of Grid members
+    stub_from: str | None = None  # list of [<fqdn>/<ip>,...]
+    stub_members: str | None = None  # list of Grid members
     ns_group: str | None = None
     ns_group_external: str | None = None
 
@@ -170,14 +194,15 @@ class StubZone(BaseModel):
 class NsGroup(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    nsgroup: str = Field(alias="header-nsgroup", default="nsgroup")
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    nsgroup: str = Field(serialization_alias="header-nsgroup", default="nsgroup")
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     group_name: str
-    new_group_name: str | None = Field(alias="_new_group_name", default=None)
-    grid_primaries: str | None = None # list of <fqdn>/<stealth_flag>
-    external_primaries: str | None = None # list of <fqdn>/<ip>/<use_2x_tsig>/<use_tsig>/<name>/<key>
-    external_secondaries: str | None = None # list of name/ip/use_2x_tsig/use_tsig/name/key
-    grid_secondaries: str | None = None # list of fqdn/stealth/lead/grid_sync
+    new_group_name: str | None = Field(serialization_alias="_new_group_name", default=None)
+    grid_primaries: str | None = None  # list of <fqdn>/<stealth_flag>
+    external_primaries: str | None = None  # list of <fqdn>/<ip>/<use_2x_tsig>/<use_tsig>/<name>/<key>
+    external_secondaries: str | None = None  # list of name/ip/use_2x_tsig/use_tsig/name/key
+    grid_secondaries: str | None = None  # list of fqdn/stealth/lead/grid_sync
     is_grid_default: bool | None = None
     comment: str | None = None
 
@@ -191,8 +216,9 @@ class NsGroup(BaseModel):
 class DelegatedZone(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    delegatedzone: str = Field(alias="header-delegatedzone", default="delegatedzone")
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    delegatedzone: str = Field(serialization_alias="header-delegatedzone", default="delegatedzone")
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     fqdn: str
     view: str | None = None
     zone_format: ZoneFormatTypeEnum
@@ -202,7 +228,7 @@ class DelegatedZone(BaseModel):
     delegate_to: str | None = None  # list of <fqdn>/<ip>
     delegated_ttl: PositiveInt | None = None
     ns_group: str | None = None
-    new_prefix: str | None = Field(alias="_new_prefix", default=None)
+    new_prefix: str | None = Field(serialization_alias="_new_prefix", default=None)
     ddns_protected: bool | None = None
     ddns_principal: str | None = None
 
@@ -216,10 +242,12 @@ class DelegatedZone(BaseModel):
 class DelegationNsGroup(BaseModel):
     model_config = ConfigDict(extra="allow")
 
-    delegationnsgroup: str = Field(alias="header-delegationnsgroup", default="delegationnsgroup")
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    delegationnsgroup: str = Field(serialization_alias="header-delegationnsgroup",
+                                   default="delegationnsgroup")
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     group_name: str
-    new_group_name: str | None = Field(alias="_new_group_name", default=None)
+    new_group_name: str | None = Field(serialization_alias="_new_group_name", default=None)
     delegate_to: str  # list of <fqdn>/<ip> servers
     comment: str | None = None
 
@@ -228,13 +256,14 @@ class ForwardingMemberNsGroup(BaseModel):
     model_config = ConfigDict(extra="allow")
 
     forwardingmembernsgroup: str = Field(
-        alias="header-forwardingmembernsgroup", default="forwardingmembernsgroup"
+        serialization_alias="header-forwardingmembernsgroup", default="forwardingmembernsgroup"
     )
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     group_name: str
-    new_group_name: str | None = Field(alias="_new_group_name", default=None)
+    new_group_name: str | None = Field(serialization_alias="_new_group_name", default=None)
     comment: str | None = None
-    forwarding_servers: str # list of use_forwarders/override_default_fwds/grid_member/[fqdn/ip]
+    forwarding_servers: str  # list of use_forwarders/override_default_fwds/grid_member/[fqdn/ip]
 
     def add_property(self, prop: str, value: str):
         if prop.startswith("EA-"):
@@ -247,13 +276,14 @@ class ForwardStubServerNsGroup(BaseModel):
     model_config = ConfigDict(extra="allow")
 
     forwardstubservernsgroup: str = Field(
-            alias="header-forwardstubservernsgroup", default="forwardstubservernsgroup"
-        )
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+        serialization_alias="header-forwardstubservernsgroup", default="forwardstubservernsgroup"
+    )
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     group_name: str
-    new_group_name: str | None = Field(alias="_new_group_name", default=None)
+    new_group_name: str | None = Field(serialization_alias="_new_group_name", default=None)
     comment: str | None = None
-    external_servers: str # list of external servers
+    external_servers: str  # list of external servers
 
     def add_property(self, prop: str, value: str):
         if prop.startswith("EA-"):
@@ -266,19 +296,17 @@ class StubMemberNsGroup(BaseModel):
     model_config = ConfigDict(extra="allow")
 
     stubmembernsgroup: str = Field(
-        alias="header-stubmembernsgroup", default="stubmembernsgroup"
+        serialization_alias="header-stubmembernsgroup", default="stubmembernsgroup"
     )
-    import_action: ImportActionEnum | None = Field(alias="import-action", default=None)
+    import_action: ImportActionEnum | None = Field(serialization_alias="import-action",
+                                                   default=None)
     group_name: str
-    new_group_name: str | None = Field(alias="_new_group_name", default=None)
+    new_group_name: str | None = Field(serialization_alias="_new_group_name", default=None)
     comment: str | None = None
-    stub_members: str # list of grid members
+    stub_members: str  # list of grid members
 
     def add_property(self, prop: str, value: str):
         if prop.startswith("EA-"):
             self.__setattr__(prop, value)
         else:
             raise Exception(f"Invalid field name: {prop}")
-
-
-
